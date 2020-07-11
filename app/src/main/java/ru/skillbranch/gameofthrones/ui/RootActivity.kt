d8a687a9243fc867616284a9e4cfae979610cb8e
IlyaShelkovenko <hostov47@gmail.com>
2020-07-11 10:00:56 +0300

    Added resources, dependencies Structure of project
    Signed-off-by: IlyaShelkovenko <hostov47@gmail.com>

---
+++ app/src/main/java/ru/skillbranch/gameofthrones/ui/RootActivity.kt
@@ -0,0 +1,17 @@
+package ru.skillbranch.gameofthrones.ui
+
+import android.graphics.drawable.Animatable
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.vectordrawable.graphics.drawable.AnimatedVectorDrawableCompat
+import kotlinx.android.synthetic.main.activity_root.*
+import ru.skillbranch.gameofthrones.R
+
+class RootActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_root)
+    }
+
+}
